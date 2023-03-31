c74462a7eb918e430a20e0801ce1ca841a5bdba9
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-03-31 20:04:33 -0300

    Config the urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from recipes.views import my_view1, my_view2, my_view3
+from recipes.views import home
 
 
 urlpatterns = [
-    path('', my_view1),
-    path('sobre/', my_view2),
-    path('contato/', my_view3)
+    path('', home),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-def my_view1(request):
-    return HttpResponse("HOME")
-
-
-def my_view2(request):
-    return HttpResponse("SOBRE")
-
-
-def my_view3(request):
-    return HttpResponse("CONTATO")
+def home(request):
+    return render(request, 'home.html')
