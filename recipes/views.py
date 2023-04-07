02dffb8adf08fb75083599140c8cefe34e4cf743
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-04-07 16:41:52 -0300

    Created Faker and apply on pages, partials and url

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(15)],
+        'is_home_page': True,
+    })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe_generator': make_recipe(),
+    })
