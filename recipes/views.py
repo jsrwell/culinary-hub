84b4e8e9f21e60a1b106d2fa7712f568fcb1592f
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-04-08 20:06:07 -0300

    Add model Recipe in view home

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(15)],
+        'recipes': recipes,
+        'is_home_page': True,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+    ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
         'is_home_page': True,
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe_generator': make_recipe(),
+        'recipe': make_recipe(),
     })
