79346bc69359966949a1476e4839e3d6683561cd
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-04-09 19:11:49 -0300

    Recipe views fixed, category page finished

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
+from django.shortcuts import render, get_object_or_404
 from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True,
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
         'is_home_page': True,
@@ -14,16 +15,16 @@ def home(request):
 def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id,
+        is_published=True,
     ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'is_home_page': True,
     })
 
-# verificar bug em 32 <a href="{% url 'recipes:category' recipe.category.id %}"> aula 59
-
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
     })
