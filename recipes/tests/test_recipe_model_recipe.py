e48df78879fb31f74dd2af9532bfeafa106d422f
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-04-12 20:17:13 -0300

    Initiate char validation

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,4 +1,5 @@
 from .test_recipe_base import RecipeTestBase
+from django.core.exceptions import ValidationError
 
 
 class RecipeModelTest(RecipeTestBase):
@@ -6,6 +7,7 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_the_test(self):
-        recipe = self.recipe
-        ...
+    def test_recipe_title_erro_if_more_than_65_chars(self):
+        self.recipe.title = "C" * 66
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
