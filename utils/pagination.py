ebab60dd4f66d15ac191c8fdd0937e0eac1dce78
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-04-17 15:32:57 -0300

    pagination TDD for first and last range

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,6 +1,23 @@
+import math
+
+
 def make_pagination_range(
     page_range,
     qty_pages,
     current_page,
 ):
-    return [1, 2, 3, 4]
+    # Check the integral list len if the list is lower than quantity call
+    if len(page_range) <= qty_pages:
+        return page_range
+
+    # Create the pagination parametrization based on data entry
+    middle = math.ceil(qty_pages / 2)
+    start = 0
+    if current_page > page_range[-1] - middle:
+        # Case are in the last pages
+        start = page_range[-1] - qty_pages
+    elif current_page >= middle:
+        # Case are in the initial pages
+        start = current_page - middle
+    stop = start + qty_pages
+    return page_range[start:stop]
--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -11,5 +11,43 @@ class PaginationTest(TestCase):
         )
         self.assertEqual([1, 2, 3, 4], pagination)
 
-    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):
-        ...
+    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):  # noqa E501
+        pagination = make_pagination_range(
+            # For page 2 in list of 4
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=2,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        pagination = make_pagination_range(
+            # For page 10 in list of 5
+            page_range=list(range(1, 21)),
+            qty_pages=5,
+            current_page=10,
+        )
+        self.assertEqual([8, 9, 10, 11, 12], pagination)
+
+        pagination = make_pagination_range(
+            # For less page than qty
+            page_range=list(range(1, 5)),
+            qty_pages=11,
+            current_page=3,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        pagination = make_pagination_range(
+            # For the last page
+            page_range=list(range(1, 11)),
+            qty_pages=4,
+            current_page=10,
+        )
+        self.assertEqual([7, 8, 9, 10], pagination)
+
+        pagination = make_pagination_range(
+            # For page 14 in 4 of a higher list
+            page_range=list(range(1, 31)),
+            qty_pages=4,
+            current_page=14,
+        )
+        self.assertEqual([13, 14, 15, 16], pagination)
