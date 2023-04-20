d3a99696e3fbb0d0168e6188ecff3a49e658e588
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-04-20 00:21:26 -0300

    Add new funcs to automate add placeholders

--- authors/forms.py
+++ authors/forms.py
@@ -2,7 +2,35 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
+
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['first_name'], 'Ex.: Well')
+        add_placeholder(self.fields['last_name'], 'Ex.: Jackson')
+        add_placeholder(self.fields['username'], 'Ex.: yourusername0945')
+        add_placeholder(self.fields['email'], 'Ex.: youremail@typed.here')
+        add_placeholder(self.fields['password'], 'Type your password here')
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput({
+            "placeholder": "Your password must be the same"
+        }),
+        label='Confirm your password:',
+        error_messages={
+            'required': "The password don't match"
+        },
+    )
+
     class Meta:
         model = User
         fields = [
@@ -12,3 +40,15 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+
+        labels = {
+            'first_name': 'First Name:',
+            'last_name': 'Last Name:',
+            'username': 'Username:',
+            'email': 'E-mail:',
+            'password': 'Password:',
+        }
+
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
