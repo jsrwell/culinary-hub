6714219e9008413ab2fc80b90a57a2b67f0f5863
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-04-23 21:18:37 -0300

    test and fix help text in fields form

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -4,6 +4,7 @@ from parameterized import parameterized
 
 
 class AuthorRegisterUnitTest(TestCase):
+    # tests to placeholders in the inputs
     @parameterized.expand([
         ('first_name', 'Ex.: Well'),
         ('last_name', 'Ex.: Jackson'),
@@ -12,7 +13,27 @@ class AuthorRegisterUnitTest(TestCase):
         ('password', 'Type your password here'),
         ('password2', 'The password must be the same'),
     ])
-    def test_first_name_placeholder_is_correct(self, field, expected_placeholder):
+    def test_fields_placeholder_is_correct(self, field, expected_placeholder):
         form = RegisterForm()
         placeholder = form[field].field.widget.attrs['placeholder']
         self.assertEqual(placeholder, expected_placeholder)
+
+    # tests to help text in the inputs
+    @parameterized.expand([
+        ('first_name', ''),
+        ('last_name', ''),
+        ('username', (
+            'Obrigatório. 150 caracteres ou menos. Letras, números e @/./+/-/_ apenas.'  # noqa E501
+        )),
+        ('email', ''),
+        ('password', (
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        )),
+        ('password2', ''),
+    ])
+    def test_fields_help_text_is_correct(self, field, expected_help_text):
+        form = RegisterForm()
+        help_text = form[field].field.help_text
+        self.assertEqual(help_text, expected_help_text)
