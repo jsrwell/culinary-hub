e5fcc38b0192350c0276bba442d802238f7edb3a
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-05-01 17:21:46 -0300

    Add views.py fix

--- authors/views.py
+++ authors/views.py
@@ -66,6 +66,7 @@ def login_create(request):
 
     return redirect(login_url)
 
+
 @login_required(login_url='authors:login', redirect_field_name='next')
 def logout_view(request):
     if not request.POST:
