3758807b2e8db18ead2d5ac47c27a2ad5e96f0ad
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-05-01 16:37:54 -0300

    Add login system

---
+++ authors/forms/__init__.py
@@ -0,0 +1,2 @@
+from .register_form import RegisterForm  # noqa F501
+from .login import LoginForm  # noqa F501
--- authors/views.py
+++ authors/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import LoginForm, RegisterForm
 from django.http import Http404
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.urls import reverse
 
 
@@ -10,7 +11,7 @@ def register_view(request):
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
-        'form_action': reverse('authors:create'),
+        'form_action': reverse('authors:register_create'),
     })
 
 
@@ -30,3 +31,35 @@ def register_create(request):
         del (request.session['register_form_data'])
 
     return redirect('authors:register')
+
+
+def login_view(request):
+    form = LoginForm()
+    return render(request, 'authors/pages/login.html', {
+        'form': form,
+        'form_action': reverse('authors:login_create')
+    })
+
+
+def login_create(request):
+    if not request.POST:
+        raise Http404()
+
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+
+        if authenticated_user is not None:
+            messages.success(request, 'Your are logged in.')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'Invalid credentials')
+    else:
+        messages.error(request, 'Invalid username or password')
+
+    return redirect(login_url)
