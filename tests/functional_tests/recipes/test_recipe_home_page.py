307785377383a4ed56e45416baeb7a8b526a9f12
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-05-06 19:18:00 -0300

    Config pytest and markers to functional tests

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,7 +1,9 @@
 from selenium.webdriver.common.by import By
+import pytest
 from tests.functional_tests.recipes.base import RecipeBaseFunctionalTest
 
 
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipes_show_not_found_recipes(self):
         browser = self.browser
@@ -12,6 +14,5 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_the_developer(self):
         browser = self.browser
         browser.get(self.live_server_url)
-        # self.sleep()
         body = browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('Well Jackson', body.text)
