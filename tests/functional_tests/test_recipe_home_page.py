8ee0f6af5683bbc018eebc2004917209e7b17b95
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-05-05 22:20:53 -0300

    Setrup and Teardown config on fuctional tests

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -5,14 +5,29 @@ from utils.browser import make_chrome_browser
 from selenium.webdriver.common.by import By
 
 
-class RecipeHomePageFunctionalTest(StaticLiveServerTestCase):
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = make_chrome_browser()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
     def sleep(self, seconds=5):
         time.sleep(seconds)
 
-    def test_the_test(self):
-        browser = make_chrome_browser()
+
+class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+    def test_recipe_home_page_without_recipes_show_not_found_recipes(self):
+        browser = self.browser
         browser.get(self.live_server_url)
-        self.sleep(6)
         body = browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('Recipes not found... 😢', body.text)
-        browser.quit()
+
+    def test_the_developer(self):
+        browser = self.browser
+        browser.get(self.live_server_url)
+        self.sleep()
+        body = browser.find_element(By.TAG_NAME, 'body')
+        self.assertIn('Well Jackson', body.text)
