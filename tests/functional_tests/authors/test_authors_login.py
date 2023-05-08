dd98591ffa29cf905da87cf7b02ddf975d0b5e0d
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-05-07 21:03:16 -0300

    Aditional tests is created with the coverage help

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -34,3 +34,61 @@ class AuthorsLoginTest(AuthorsBaseTest):
             f'Your are logged in with {user.username}.',
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
+
+    def test_login_create_raises_404_if_not_POST_method(self):
+        self.browser.get(
+            self.live_server_url +
+            reverse('authors:login_create')
+        )
+        self.assertIn(
+            'Not Found',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+    def test_form_login_is_invalid(self):
+        # User open the login page
+        self.browser.get(
+            self.live_server_url + reverse('authors:login')
+        )
+
+        # User find the login form
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        # User try send the form without type anything
+        username = self.get_by_placeholder(form, 'Type your username')
+        password = self.get_by_placeholder(form, 'Type your password')
+        username.send_keys(' ')
+        password.send_keys(' ')
+
+        # The form is send
+        form.submit()
+
+        # User see the erro message in screen
+        self.assertIn(
+            'Invalid username or password',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+    def test_form_login_invalid_credentials(self):
+        # User open the login page
+        self.browser.get(
+            self.live_server_url + reverse('authors:login')
+        )
+
+        # The login form is find
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        # The user send the invalid credentials
+        username = self.get_by_placeholder(form, 'Type your username')
+        password = self.get_by_placeholder(form, 'Type your password')
+        username.send_keys('invalid_user')
+        password.send_keys('invalid_password')
+
+        # The form is send
+        form.submit()
+
+        # The credential erros is visualized in screen
+        self.assertIn(
+            'Invalid credentials',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
