539f5480f5693ebdfe12e98f1d0539791591b4cb
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-06-06 22:23:34 -0300

    recipe list class apply

--- recipes/views.py
+++ recipes/views.py
@@ -2,11 +2,41 @@ import os
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.views.generic import ListView
 from .models import Recipe
 from utils.pagination import make_pagination
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 9))
 PAGINATOR_SIZE = int(os.environ.get('PAGINATOR_SIZE', 3))
+
+
+class RecipeListViewBase(ListView):
+    model = Recipe
+    context_object_name = 'recipes'
+    ordering = ['-id']
+    template_name = 'recipes/pages/home.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            is_published=True,
+        )
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_obj, pagination_range = make_pagination(
+            self.request,
+            ctx.get('recipes'),
+            PER_PAGE,
+            PAGINATOR_SIZE
+        )
+        ctx.update(
+            {'recipes': page_obj,
+             'pagination_range': pagination_range,
+             'is_home_page': True, }
+        )
+        return ctx
 
 
 def home(request):
