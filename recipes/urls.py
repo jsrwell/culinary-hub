70fb5f5ae753a7b6eeb46de5c8a0ccbee730dc74
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-06-08 21:03:02 -0300

    Class category add context, clean defs

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,9 +4,20 @@ from . import views
 app_name = "recipes"
 
 urlpatterns = [
-    path('', views.RecipeListViewHome.as_view(), name="home"),
-    path('recipes/search/', views.RecipeListViewSearch.as_view(), name="search"),
+    path('',
+         views.RecipeListViewHome.as_view(),
+         name="home"
+         ),
+    path('recipes/search/',
+         views.RecipeListViewSearch.as_view(),
+         name="search"
+         ),
+    path('recipes/<int:id>/',
+         views.recipe,
+         name="recipe"
+         ),
     path('recipes/category/<int:category_id>/',
-         views.RecipeListViewCategory.as_view(), name="category"),
-    path('recipes/<int:id>/', views.recipe, name="recipe"),
+         views.RecipeListViewCategory.as_view(),
+         name="category"
+         ),
 ]
